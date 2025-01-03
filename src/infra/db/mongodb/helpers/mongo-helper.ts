b9d1845140a13b04ff4897acf2f6500de33474ed
weesleycabral <weesleylima@hotmail.com>
2025-01-03 09:30:23 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,16 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
   db: null as any,
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__, {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MONGO_URL is not defined')
+    }
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
     })
     this.db = await this.client.db()
   },
 
   async disconnect () {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.db.collection(name)
   }
 }
